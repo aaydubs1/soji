1361ad8baf6505670784aa229d20a9d1e0d285bf
Aaydubs <albab1551@gmail.com>
2025-12-19 14:52:57 +0100

    añadido un boton al Intro-article
    efecto expand añadido none en js para que salga un fondo blanco

--- js/index.js
+++ js/index.js
@@ -1,12 +1,16 @@
 'use strict'
 
 const expandBox = document.querySelector(`.Intro-article`)
+const contentBox = document.querySelector(`.Intro-article--wrapper`)
+console.log(contentBox)
 
 // Agregar listener que se ejecuta cada vez que haces scroll
 window.addEventListener('scroll', () => {
-    if(window.scrollY > 0){
+    if(window.scrollY > 20){
         expandBox.classList.add(`expand`)
+        contentBox.classList.add(`none`)
     }else{
         expandBox.classList.remove(`expand`)
+        contentBox.classList.remove(`none`)
     }
 })
